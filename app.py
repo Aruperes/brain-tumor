937a9fee3fd1747a2dd7d1741d920a982103e03a
Nancimento <revandoaruperes6@gmail.com>
2025-09-04 00:22:58 +0800

    fix

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import numpy as np
 from ultralytics import YOLO
 import google.generativeai as genai
 import tensorflow as tf
-from tensorflow.keras.models import load_model
 import cv2
 import matplotlib
 import markdown
