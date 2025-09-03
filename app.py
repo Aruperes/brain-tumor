27c3a45c7483742af5af6700fde6a59506e07873
Nancimento <revandoaruperes6@gmail.com>
2025-09-03 21:23:27 +0800

    modif req

--- app.py
+++ app.py
@@ -251,4 +251,7 @@ def segment():
     )
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # Port akan diambil dari variabel lingkungan PORT yang disediakan oleh Railway
+    port = int(os.environ.get("PORT", 5000))
+    # Set debug=False untuk lingkungan produksi
+    app.run(host="0.0.0.0", port=port, debug=False)
